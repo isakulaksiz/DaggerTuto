4c62975c682cd459f53c280f8703f273fb1c97eb
isa kulaksız <>
2022-06-16 20:53:36 +0300

    added app module

--- app/src/main/java/com/example/daggertuto/MainActivity.kt
+++ app/src/main/java/com/example/daggertuto/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.daggertuto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
